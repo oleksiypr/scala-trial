c7f824edd324bad5ff8830ee4a6dbe68e970e9ba
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2026-01-16 15:23:40 +0100

    [AsyncRest] Added HEAD /job trivial API implementation: added count header

--- src/main/scala/async/rest/AsyncJobApi.scala
+++ src/main/scala/async/rest/AsyncJobApi.scala
@@ -4,9 +4,12 @@ import org.http4s.*
 import org.http4s.dsl.io.*
 import org.http4s.implicits.*
 import cats.effect.IO
+import org.typelevel.ci.CIString
+import org.typelevel.ci.*
 
 object AsyncJobApi {
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case HEAD -> Root / "jobs" => Accepted()
+    case HEAD -> Root / "jobs" =>
+      Accepted().map(_.putHeaders(Header.Raw(ci"X-Total-Count", "42")))
   }
 }
--- src/test/scala/async/rest/AsyncJobApiSpec.scala
+++ src/test/scala/async/rest/AsyncJobApiSpec.scala
@@ -1,24 +1,25 @@
 package async.rest
 
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpecLike
-import org.http4s.*
-import org.http4s.dsl.io.*
-import org.http4s.implicits.*
 import cats.effect.IO
-import org.mockito.Mockito.*
+import cats.effect.testing.scalatest.AsyncIOSpec
+import org.http4s.*
+import org.http4s.implicits.*
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
 import org.scalatestplus.mockito.MockitoSugar
-import cats.effect.unsafe.implicits.global
-import org.typelevel.ci.CIString
+import org.typelevel.ci.*
 
-class AsyncJobApiSpec extends AnyWordSpecLike with Matchers {
+class AsyncJobApiSpec extends AsyncWordSpec
+  with AsyncIOSpec with Matchers with MockitoSugar {
+
   "AsyncJobApi" should {
     "handle HEAD request which returns count of job items" in {
       val request = Request[IO](Method.HEAD, uri"/jobs")
-      val response = AsyncJobApi.routes.orNotFound.run(request).unsafeRunSync()
-      response.status shouldEqual Status.Accepted
+      val response = AsyncJobApi.routes.orNotFound.run(request)
+      response.asserting { resp =>
+        resp.status shouldBe Status.Accepted
+        resp.headers.get(ci"X-Total-Count").map(_.head.value) shouldBe Some("42")
+      }
     }
   }
 }
-
-
