68375eaaa7404897ae4783349a80aeeb600c2f3d
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2026-02-27 22:34:39 +0100

    [progmeta] Enhance type-labeling utility and `describeClass` functionality: - Add `GetLabel` type class for basic types - Implement methods to extract labels and types for product elements - Introduce `describeClass` for detailed product class descriptions with labeled types - Refine `Debug.included` example with new functionality

--- src/main/scala/progmeta/Main.scala
+++ src/main/scala/progmeta/Main.scala
@@ -1,6 +1,7 @@
 package progmeta
 
-import cats.Id
+import cats.{Eq, Id, Show}
+import dotty.tools.backend.jvm.Primitives.Primitive
 
 import scala.compiletime.{constValue, constValueTuple}
 
@@ -29,31 +30,72 @@ object Main {
       type Record = (Int, String, Boolean)
 
       type T[x] = x match
-        case Int => 0
-        case String => 1
+        case Int     => 0
+        case String  => 1
         case Boolean => 2
 
       type TupleElems[t] = t match
         case EmptyTuple => EmptyTuple
-        case h *: t => T[h] *: TupleElems[t]
+        case h *: t     => T[h] *: TupleElems[t]
 
       val ordinals = constValueTuple[TupleElems[Record]]
       println(ordinals)
     }
 
-    Debug.included(false) {
+    Debug.included(true) {
       import scala.deriving.Mirror
       import scala.compiletime.*
 
       case class Baz(n: Int, s: String, b: Boolean)
+      case class Qux(d: Double, c: Baz)
 
-      inline def names[T](using m: Mirror.ProductOf[T]): Unit = {
-        // println(constValue[m.MirroredLabel])
-        println(constValueTuple[m.MirroredElemLabels])
+      trait GetLabel[T]:
+        def label: String
+
+      given GetLabel[Int] with
+        def label: String = "Int"
+
+      given GetLabel[String] with
+        def label: String = "String"
+
+      given GetLabel[Boolean] with
+        def label: String = "Boolean"
+
+      given GetLabel[Double] with
+        def label: String = "Double"
+
+      inline def label[T <: Product](using m: Mirror.ProductOf[T]): String =
+        constValue[m.MirroredLabel]
+
+      inline def elementLabels[T <: Product](using m: Mirror.ProductOf[T]): List[String] =
+        constValueTuple[m.MirroredElemLabels].toList.map(_.toString)
+
+      inline def typeLabelsProduct[T <: Product](
+          using m: Mirror.ProductOf[T]
+        ): List[String] = typeLabelsTuple[m.MirroredElemTypes]
+
+      inline def typeLabelsTuple[T <: Tuple]: List[String] =
+        inline erasedValue[T] match
+          case _: EmptyTuple      => Nil
+          case _: (elem *: elems) => labelElem[elem] :: typeLabelsTuple[elems]
+
+      inline def labelElem[Elem]: String =
+        summonFrom {
+          case m: Mirror.ProductOf[Elem] => constValue[m.MirroredLabel]
+          case _                         => summonInline[GetLabel[Elem]].label
+        }
+
+
+      inline def describeClass[T <: Product](using m: Mirror.ProductOf[T]): String = {
+        val className        = label[T]
+        val elemLabels      = elementLabels[T]
+        val typeLabels      = typeLabelsProduct[T]
+        val labelsWithTypes = elemLabels.zip(typeLabels).map( (e, t) => s"$e: $t").mkString(", ")
+        s"$className($labelsWithTypes)"
       }
 
-      names[Baz]
+      println(describeClass[Baz])
+      println(describeClass[Qux])
     }
-
   }
 }
