5af89483f95f61b3bad9570983feb8384a26e047
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2026-03-07 13:45:32 +0100

    [progmeta] Add `Baz` case class and extend `ReprSpec` tests for new cases: - Introduce `Baz` with `Int` and `Double` fields - Add tests for `Repr` functionality for `Baz` and adjust `Bar` tests

--- src/test/scala/progmeta/ReprSpec.scala
+++ src/test/scala/progmeta/ReprSpec.scala
@@ -7,6 +7,7 @@ import org.scalatest.matchers.should.Matchers.shouldBe
 object ReprSpec {
   case class Qux() derives Repr
   case class Bar(m: Double) derives Repr
+  case class Baz(n: Int, m: Double)
   case class Foo(n: Int, bar: Bar) derives Repr
 }
 
@@ -18,6 +19,16 @@ class ReprSpec extends AnyFunSuite with Matchers {
   test("Repr for Qux()") {
     val qux = Qux()
     qux.repr shouldBe "Qux()"
+  }
+
+  test("Repr for Bar(2.0)") {
+    val bar = Bar(2.0)
+    bar.repr shouldBe "Bar(m: Double = 2.0)"
+  }
+
+  test("Repr for Baz(1, 2.0)") {
+    val bar = Baz(1, 2.0)
+    bar.repr shouldBe "Baz(n: Int = 1, m: Double = 2.0)"
   }
   
   test("Repr for Foo(1, Bar(2.0))") {
