ad84cdd148aeeeb5ea62bf345c66ab8152206560
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2026-03-10 17:15:49 +0100

    [progmeta] [new] Add `repr` extension method and basic `derived` implementation: - Introduce `repr` extension method to simplify accessing representations. - Implement basic `derived` method for `Repr`, defaulting to `toString`. - Add test for `Repr` functionality with `Foo` case class.

--- src/main/scala/progmeta/Repr.scala
+++ src/main/scala/progmeta/Repr.scala
@@ -9,5 +9,13 @@ trait Repr[T] {
 
 
 object Repr {
-  inline def derived[T](using m: Mirror.Of[T]): Repr[T] = ???
+  extension [T](t: T) {
+    def repr(using r: Repr[T]): String = r.repr(t)
+  }
+
+  inline def derived[T](using m: Mirror.Of[T]): Repr[T] =
+    new Repr[T] {
+      override def repr(t: T): String = t.toString
+      override def label: String = m.toString
+    }
 }
--- src/test/scala/progmeta/ReprSpec.scala
+++ src/test/scala/progmeta/ReprSpec.scala
@@ -3,12 +3,19 @@ package progmeta
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+
 object ReprSpec {
-  case class Foo() derives Repr  
+  case class Foo() derives Repr
 }
 
 
 class ReprSpec extends AnyFunSuite with Matchers {
-  
+  import ReprSpec.*
+  import Repr.*
+
+  test("Repr for Foo()") {
+    val qux = Foo()
+    qux.repr shouldBe "Foo()"
+  }
 
 }
