ede281665c8c2716491bd4abfcb9fc57afba0d91
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2026-03-13 16:53:34 +0100

    [progmeta][new] Actually no test requires `Repr#label`! This is a copy-paste from ReprSpec. Hence remove unused `label` method from `Repr` trait and implementations

--- src/main/scala/progmeta/Repr.scala
+++ src/main/scala/progmeta/Repr.scala
@@ -5,7 +5,6 @@ import scala.compiletime.{constValue, constValueTuple, summonFrom, erasedValue, 
 
 trait Repr[T] {
   def repr(t: T): String
-  def label: String
 }
 
 object Repr {
@@ -13,21 +12,19 @@ object Repr {
     def repr(using r: Repr[T]): String = r.repr(t)
   }
 
-  inline def derived[T](using m: Mirror.Of[T]): Repr[T] = 
+  inline def derived[T](using m: Mirror.Of[T]): Repr[T] =
     val label = constValue[m.MirroredLabel]
     inline m match
       case mp: Mirror.ProductOf[T] => productRepr[T](using mp)
       case _: Mirror.Of[T]         => sumRepr[T]
-  
+
   private def productRepr[T](using mp: Mirror.ProductOf[T]): Repr[T] =
     new Repr[T] {
       override def repr(t: T): String = "Foo()"
-      override def label: String = "Foo"
     }
 
   private def sumRepr[T]: Repr[T] =
     new Repr[T] {
       override def repr(t: T): String = "Some(value: Boolean = true)"
-      override def label: String = "Option"
     }
 }
