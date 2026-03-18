cbf3fdd6a6fb1be57607d5e0348a710262790af0
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2026-03-18 15:39:40 +0100

    [progmeta][new] Add type class mapping and evaluation tests in `ReprSpec`.
    - Introduced `Ev` type class for tuple elements.
    - Added tests to verify tuple mapping, zipping with type class instances, and evaluation.

--- src/test/scala/progmeta/ReprSpec.scala
+++ src/test/scala/progmeta/ReprSpec.scala
@@ -88,5 +88,53 @@ class ReprSpec extends AnyFunSuite with Matchers {
     val tuple: MyTuple = (1, "hi", true)
     val mapped: MappedTuple = tuple.map([t] => t => TupMap[t](t))
     mapped shouldBe (TupMap(1), TupMap("hi"), TupMap(true))
+    mapped(0) shouldBe TupMap(1)
+    mapped(1) shouldBe TupMap("hi")
+    mapped(2) shouldBe TupMap(true)
+    def ord = 1
+    mapped(ord) shouldBe TupMap("hi")
+  }
+
+  test("Typce class mapping") {
+    trait Ev[T] {
+      def foo(t: T): String
+    }
+
+    val evInt : Ev[Int]     = (t: Int)     => s"ev($t: Int)"
+    val evStr : Ev[String]  = (t: String)  => s"ev($t: String)"
+    val evBool: Ev[Boolean] = (t: Boolean) => s"ev($t: Boolean)"
+
+    type MyTuple = (Int, String, Boolean)
+
+    type Evs[Tup <: Tuple]     = Tuple.Map[Tup, Ev]
+    type WithEvs[Tup <: Tuple] = Tuple.Zip[Tup, Evs[Tup]]
+
+    val tuple: MyTuple = (1, "hi", true)
+    val evs: Evs[MyTuple] = (evInt, evStr, evBool)
+
+    val zipped: WithEvs[MyTuple] = tuple.zip(evs)
+
+    def check[Tup1 <: Tuple, Tup2](using Tup1 =:= Tup2): Unit = ()
+
+    check[
+      Tuple.Map[MyTuple, [t] =>> (t, String)],
+      ((Int, String), (String, String), (Boolean, String))
+    ]
+
+    check[
+      WithEvs[MyTuple],
+      ((Int, Ev[Int]), (String, Ev[String]), (Boolean, Ev[Boolean]))
+    ]
+
+    inline def eval[Tup <: Tuple](zipped: WithEvs[Tup]): Tuple.Map[Tup, [t] =>> (t, String)] =
+      inline scala.compiletime.erasedValue[Tup] match
+        case _: EmptyTuple => EmptyTuple
+        case _: (h *: t) =>
+          val head = zipped.asInstanceOf[(h, Ev[h]) *: WithEvs[t]].head
+          val tail = zipped.asInstanceOf[(h, Ev[h]) *: WithEvs[t]].tail
+          (head._1, head._2.foo(head._1)) *: eval[t](tail)
+
+    eval[MyTuple](zipped) shouldBe
+      ((1, "ev(1: Int)"), ("hi", "ev(hi: String)"), (true, "ev(true: Boolean)"))
   }
 }
